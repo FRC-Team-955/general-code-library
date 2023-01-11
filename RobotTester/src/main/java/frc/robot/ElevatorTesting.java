9ff3323df9bab999bde70409e134618a04e1369c
ThaiPituk <97065922+ThaiPituk@users.noreply.github.com>
2023-01-10 19:50:43 -0800

    Added Elevator Testers
    Comes with both Falcon and Neo testers. Simply runs the elevator.

---
+++ RobotTester/src/main/java/frc/robot/ElevatorTesting.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType; 
+import com.revrobotics.CANSparkMax.IdleMode; 
+import com.revrobotics.CANSparkMaxLowLevel.PeriodicStatus2; 
+
+public class ElevatorTesting {
+private TalonFX ElevatorMotor; 
+private CANSparkMax ElevatorMotorN;
+
+ElevatorTesting(int Elevator_MotorID)
+{
+TalonFX Elevator_Motor = new TalonFX(Elevator_MotorID);
+ElevatorMotor = Elevator_Motor; 
+
+ElevatorMotor.setNeutralMode(NeutralMode.Brake); 
+ElevatorMotor.setSelectedSensorPosition(0); 
+
+SupplyCurrentLimitConfiguration config = new SupplyCurrentLimitConfiguration(true, 40,40,0);
+ElevatorMotor.configSupplyCurrentLimit(config, 100);
+
+CANSparkMax Elevator_MotorN = new CANSparkMax(Elevator_MotorID, MotorType.kBrushless); 
+ElevatorMotorN = Elevator_MotorN; 
+
+ElevatorMotorN.setIdleMode(IdleMode.kBrake);
+ElevatorMotorN.setSmartCurrentLimit(40); 
+
+}
+
+void ElevatorMove(Joystick joy0)
+{
+double joystick_position = joy0.getRawAxis(1);
+joystick_position = -joystick_position;
+
+ElevatorMotor.set(ControlMode.PercentOutput, joystick_position);
+ElevatorMotorN.set(joystick_position);
+};
+
+};
